29cf356e4878a65131c94284748101b0cb455a94
Karmura <kaungkhantkyaw408@gmail.com>
2023-01-11 09:46:42 +0630

    Exercise 1

---
+++ TestIncrement.java
@@ -0,0 +1,17 @@
+package Exercises;
+
+public class TestIncrement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10;    // Declare the value of i
+		int iInc; // Declare the value of iInc
+		
+		iInc = i++; // the result of iInc = i++
+		System.out.println("The result of i++ is " + iInc);
+		
+		iInc = ++i; // the result of iInc = ++i
+		System.out.println("The result of ++i is " + iInc);
+	}	
+
+}
